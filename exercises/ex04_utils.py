2cf90dff47f222a14ed0e51b2efcb9de5684dbff
Suhyun Lee <selee@unc.edu>
2023-10-07 15:01:22 -0400

    ex04 second draft

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,8 +2,12 @@
 
 __author__: str = "730468571"
 
+
 def all(int_list: list[int], searched_int: int) -> bool:
-    """Returns bool indicating whether int is same as all ints in list"""
+    """Returns bool indicating whether int is same as all ints in list."""
+    # empty list
+    if len(int_list) == 0:
+        raise ValueError ("all() arg is an empty List")
     list_idx: int = 0
     # all numbers in list don't match int
     while list_idx < len(int_list):
@@ -13,8 +17,10 @@ def all(int_list: list[int], searched_int: int) -> bool:
     # all numbers in list do match int
     return True
 
+
 def max(input: list[int]) -> int:
-    """Given a list of ints, returns largest in List"""
+    """Given a list of ints, returns largest in List."""
+    # empty list
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     list_idx: int = 0
@@ -27,9 +33,13 @@ def max(input: list[int]) -> int:
         list_idx += 1
     return max_number
 
+
 def is_equal(list1: list[int], list2: list[int]) -> bool:
-    """Given two lists of int values, return True if every element at every index is equal in both lists"""
+    """Given two lists of int values, return True if every element at every index is equal in both lists."""
     list_idx: int = 0
+    # empty list
+    if len(list1) == 0 or len(list2) == 0:
+        raise ValueError("is_equal() arg is an empty List")
     # matching each element at every index
     while list_idx < len(list1) and list_idx < (len(list2)):
         if len(list1) != len(list2):
