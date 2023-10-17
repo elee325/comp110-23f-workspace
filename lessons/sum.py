5bec2d062cf1a63f6bea0e39e9fb7e7107adb92c
Suhyun Lee <selee@unc.edu>
2023-10-17 10:32:19 -0400

    for in loop practice again

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,8 +1,9 @@
 """Summing the elements of a list using different loops."""
 __author__: str = "730468571"
 
+
 def w_sum(vals: list[float]) -> float:
-    """returns sum using while loop"""
+    """Returns sum using while loop."""
     idx: int = 0
     sum: float = 0.0
     if len(vals) == 0:
@@ -12,17 +13,18 @@ def w_sum(vals: list[float]) -> float:
         idx += 1
     return sum
 
+
 def f_sum(vals: list[float]) -> float:
-    """returns sum using for in loop"""
+    """Returns sum using for in loop."""
     sum: float = 0
     for num in vals:
         sum += num
     return sum
 
+
 def f_range_sum(vals: list[float]) -> float:
-    """returns sum using for in range loop"""
+    """Returns sum using for in range loop."""
     sum: float = 0
     for idx in range(0, len(vals)):
         sum += vals[idx]
     return sum
-
