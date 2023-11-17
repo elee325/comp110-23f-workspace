b642b14c347588faa26f8765b89566171cebc260
Suhyun Lee <selee@unc.edu>
2023-11-16 19:04:07 -0500

    recursion issues

--- lessons/recursion/node.py
+++ lessons/recursion/node.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """Node Class."""
 
 from __future__ import annotations
@@ -31,26 +30,3 @@ class Node:
     
     def last(self):
         return None
-=======
-"""Node class for a linked list."""
-
-from __future__ import annotations
-
-class Node:
-    
-    data: int
-    next: Node | None
-
-    def __init__(self, data: int, next: Node | None):
-        """Construct a node."""
-        self.data = data
-        self.next = next
-
-    def __str__(self) -> str:
-        if self.next is None:
-            # base case
-            return str(self.data)
-        else:
-            # recursive step
-            return str(self.data) + "->" + str(self.next)
->>>>>>> 0578a44 (class practice)
